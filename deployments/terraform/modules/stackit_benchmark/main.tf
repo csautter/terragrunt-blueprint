b6b3e6a7dd4101ec6bf439062fefba34f72b0eb1
Carl-Christian Sautter <carl-christian.sautter@novatec-gmbh.de>
2024-12-15 21:11:06 +0100

    chore: stackit benchmark module - accept list of instances

--- deployments/terraform/modules/stackit_benchmark/main.tf
+++ deployments/terraform/modules/stackit_benchmark/main.tf
@@ -1,6 +1,5 @@
 locals {
-  ssh_public_key_path  = "~/.ssh/id_ed25519.pub"
-  ssh_private_key_path = "~/.ssh/id_ed25519"
+
 }
 
 resource "stackit_key_pair" "keypair" {
@@ -9,16 +8,17 @@ resource "stackit_key_pair" "keypair" {
 }
 
 resource "stackit_server" "bench" {
+  for_each          = toset(local.machine_types)
   project_id        = var.project_id
-  availability_zone = "eu01-1"
+  availability_zone = local.availability_zones[0]
   boot_volume = {
     size              = 64
     source_type       = "image"
     source_id         = "59838a89-51b1-4892-b57f-b3caf598ee2f"
     performance_class = "storage_premium_perf1"
   }
-  name         = "bench-${replace(var.env, "_", "-")}"
-  machine_type = "g1.1"
+  name         = "bench-${replace(var.env, "_", "-")}-${each.value}"
+  machine_type = each.value
   keypair_name = stackit_key_pair.keypair.name
   user_data    = file("${path.module}/cloud-init.yaml")
 
@@ -26,19 +26,27 @@ resource "stackit_server" "bench" {
 }
 
 resource "null_resource" "provision" {
+  for_each = toset(local.machine_types)
+
   connection {
     type        = "ssh"
     user        = "ubuntu"
     private_key = file(local.ssh_private_key_path)
-    host        = stackit_public_ip.public_ip.ip
+    host        = stackit_public_ip.public_ip[each.key].ip
   }
 
   provisioner "remote-exec" {
     script = "${path.module}/provision.sh"
   }
 
+  provisioner "remote-exec" {
+    inline = [
+      "jq '.provider.name = \"stackit\" | .provider.instance_type = \"${each.value}\" | .provider.availability_zone = \"${local.availability_zones[0]}\"' /tmp/benchmark.json | sponge /tmp/benchmark.json"
+    ]
+  }
+
   provisioner "local-exec" {
-    command = "mkdir -p ${path.module}/bench/ && scp -o StrictHostKeyChecking=no -i ${local.ssh_private_key_path} ubuntu@${stackit_public_ip.public_ip.ip}:/tmp/benchmark.json ${path.module}/bench/${stackit_server.bench.machine_type}.json"
+    command = "mkdir -p ${path.module}/bench/ && scp -o StrictHostKeyChecking=no -i ${local.ssh_private_key_path} ubuntu@${stackit_public_ip.public_ip[each.key].ip}:/tmp/benchmark.json ${path.module}/bench/${timestamp()}-${local.availability_zones[0]}-${each.value}.json"
   }
 
   depends_on = [stackit_server.bench, stackit_server_network_interface_attach.nic_attachment]
@@ -52,22 +60,23 @@ resource "stackit_network" "network" {
 }
 
 resource "stackit_network_interface" "nic" {
+  for_each           = toset(local.machine_types)
   project_id         = var.project_id
   network_id         = stackit_network.network.network_id
   security_group_ids = [stackit_security_group.this.security_group_id]
 }
 
 resource "stackit_public_ip" "public_ip" {
+  for_each             = toset(local.machine_types)
   project_id           = var.project_id
-  network_interface_id = stackit_network_interface.nic.network_interface_id
+  network_interface_id = stackit_network_interface.nic[each.key].network_interface_id
 }
 
 resource "stackit_server_network_interface_attach" "nic_attachment" {
+  for_each             = toset(local.machine_types)
   project_id           = var.project_id
-  server_id            = stackit_server.bench.server_id
-  network_interface_id = stackit_network_interface.nic.network_interface_id
-
-
+  server_id            = stackit_server.bench[each.key].server_id
+  network_interface_id = stackit_network_interface.nic[each.key].network_interface_id
 }
 
 resource "stackit_security_group" "this" {
